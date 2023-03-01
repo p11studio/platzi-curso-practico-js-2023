afe21a0f310172d94c4985e3fe2dcb0f290f85a1
Hugo Ponce <p11studyo@gmail.com>
2023-03-01 16:35:02 -0600

    se agrega archivo array-objetos.js

---
+++ arrays-objetos.js
@@ -0,0 +1,8 @@
+function imprimirPrimerElementoArray(arr) {
+    console.log(arr[0])
+}
+ imprimirPrimerElementoArray(['Juanita', 'Rigoberta', 'Natalia'])
+
+ function imprimirPrimerElementoArray(arr) {
+    console.log(arr[1])
+}
