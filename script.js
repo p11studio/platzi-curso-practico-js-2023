00a97cf923ed3e4f69470d780a9c06e761115a49
Hugo Ponce <p11studyo@gmail.com>
2023-03-11 14:31:21 -0600

    Ejercicios de la clase 12 eventos de javascript Aqui van otros comentarios de prueba para ver que pasa cuando escribo mas texto

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
 
 const h1 = document.querySelector('h1')
-const p = document.querySelector('p')
-const parrafito = document.querySelector('.parrafito')
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1')
+const input2 = document.querySelector('#calculo2')
+const btn = document.querySelector('#btnCalcular')
+const pResult = document.querySelector('#result')
 
-console.log(input.value)
+ function btnOnClick() {
+    const sumaInputs = input1.value + input2.value
+    pResult.innerText = "Resultado: " + sumaInputs
+ }
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-})
-
-//h1.innerHTML = 'Patito <br> Feo'
-h1.innerText = 'Jordan mothafucker'
-
-
-//console.log(h1.getAttribute('class'))
-//h1.setAttribute('class', 'rojo')
-
-h1.classList.add('rojo')
-h1.classList.remove('verde')
-// h1.classList.toggle('verde')
-// h1.classList.contains('verde')
-
-input.value = '123abc'
-
-const img =  document.createElement('img');
-img.setAttribute('src', 'https://thumbs.gfycat.com/UnluckyTenseJoey-max-1mb.gif')
-
-pid.innerHTML = ''
-pid.append(img)
